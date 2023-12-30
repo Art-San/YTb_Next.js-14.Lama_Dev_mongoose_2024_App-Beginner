a3970b6073185eeff2c10dee482458aa5ee4131b
Art San <artsan.mcc@gmail.com>
2023-12-30 21:20:55 +0700

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,52 @@
+// import { Inter } from 'next/font/google'
+// import './globals.css'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata = {
+//   title: 'Next App',
+//   description: 'Next.js starter app'
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <h1>This is the main layout</h1>
+//         {children}
+//       </body>
+//     </html>
+//   )
+// }
+
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
+// import ClientSideProviderTest from "@/components/clientSideProviderTest";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
+  title: {
+    default: 'Next.js 14 Homepage',
+    template: '%s | Next.js 14'
+  },
+  description: 'Next.js starter app description'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <ClientSideProviderTest> */}
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+        {/* </ClientSideProviderTest> */}
+      </body>
     </html>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import Links from './links/Links'
+import styles from './navbar.module.css'
+// import { auth } from '@/lib/auth'
+
+const Navbar = async () => {
+  // const session = await auth();
+
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
+      <div>
+        <Links session={''} />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
+// import Link from 'next/link'
+// import styles from './navbar.module.css'
+
+// const Navbar = () => {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.logo}>logo</div>
+//       <div className={''}>
+//         <Link href={'/'}>Home</Link>
+//         <Link href={'/about'}>About</Link>
+//         <Link href={'/contact'}>Contact</Link>
+//         <Link href={'/blog'}>Blog</Link>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
