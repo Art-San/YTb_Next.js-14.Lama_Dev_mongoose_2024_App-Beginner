bb3fafeae314436d83ff812114014e2a34f5834f
Art San <artsan.mcc@gmail.com>
2024-01-01 01:02:46 +0700

    ok

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 const AboutPage = () => {
   return (
     <div>
+      <div className="">1:02:;00</div>
       {/* <img src="/about.png" alt="" /> */}
       <Image src="/about.png" alt="" width={300} height={300} />
     </div>
