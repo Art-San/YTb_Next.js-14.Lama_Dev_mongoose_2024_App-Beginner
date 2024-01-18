3dd53f0a7841066124d81be0dae10e180b138fd9
Art San <artsan.mcc@gmail.com>
2024-01-18 16:41:54 +0700

    ok

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -32,27 +32,6 @@
 
 // export default LoginPage
 
-// import LoginForm from '@/components/loginForm/loginForm'
-// import { handleGithubLogin, login } from '@/lib/action'
-// import styles from './login.module.css'
-
-// const LoginPage = () => {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.wrapper}>
-//         <form action={handleGithubLogin}>
-//           <button className={styles.github}>Login with Github</button>
-//         </form>
-//         <form className={styles.form} action={login}>
-//           <input type="text" placeholder="username" name="username" />
-//           <input type="password" placeholder="password" name="password" />
-//           <button>Login with Credentials</button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
 // export default LoginPage
 import LoginForm from '@/components/loginForm/loginForm'
 import { handleGithubLogin } from '@/lib/action'
